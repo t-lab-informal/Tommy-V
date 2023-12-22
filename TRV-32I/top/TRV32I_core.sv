4c1e41d4508e5b79354e4f80cbab2411111dc641
Shimauma01 <49707118+Shimauma01@users.noreply.github.com>
2023-12-23 00:17:35 +0900

    update top modules
    fix some ports

--- TRV-32I/top/TRV32I_core.sv
+++ TRV-32I/top/TRV32I_core.sv
@@ -1,10 +1,11 @@
-module TRV32I_top #(
+module TRV32I_core #(
     parameter B_WIDTH = 32;
 ) (
     input           clk, rst;
     output  [31:0]  pc, inst;
-    output          mem_read_en, mem_write_en;
-    output  [B_WIDTH-1:0] mem_data;
+    output  [B_WIDTH-1:0] mem_addr;
+    output                mem_read_en, mem_write_en;
+    inout   [B_WIDTH-1:0] mem_data;
 );
 
     //datapath
