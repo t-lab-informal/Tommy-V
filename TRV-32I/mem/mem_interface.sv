7c30750f32e1e5cc9f7b14d9c1dd663cd92be21f
Shimauma01 <49707118+Shimauma01@users.noreply.github.com>
2023-12-23 15:15:18 +0900

    update mem_interface
    fix in-out mistake of "inst"

--- TRV-32I/mem/mem_interface.sv
+++ TRV-32I/mem/mem_interface.sv
@@ -2,13 +2,13 @@ module mem_interface #(
     parameter B_WIDTH = 32;
     parameter MEM_SIZE = 32;
 ) (
-    input                   clk, rst;
-    input [B_WIDTH-1:0]     pc;
-    input [31:0]            inst;
-    input [B_WIDTH-1:0]     mem_addr;
-    input                   mem_read_en, mem_write_en;
-    input [(B_WIDTH/8)-1:0] write_byte_en;
-    inout [B_WIDTH-1:0]     mem_data;
+    input                       clk, rst;
+    input  [B_WIDTH-1:0]        pc;
+    input  [B_WIDTH-1:0]        mem_addr;
+    input                       mem_read_en, mem_write_en;
+    input  [(B_WIDTH/8)-1:0]    write_byte_en;
+    output [31:0]               inst;
+    inout  [B_WIDTH-1:0]        mem_data;
 );
 
     //port connect
