6084843b2db7e77c49d776c6a87d5a4afd1c7beb
Shimauma01 <49707118+Shimauma01@users.noreply.github.com>
2023-12-23 15:13:23 +0900

    update top modules
    fix in-out mistake of "inst"

--- TRV-32I/top/TRV32I_core.sv
+++ TRV-32I/top/TRV32I_core.sv
@@ -1,12 +1,11 @@
-module TRV32I_core #(
-    parameter B_WIDTH = 32;
-) (
-    input                       clk, rst;
-    output  [31:0]              pc, inst;
-    output  [B_WIDTH-1:0]       mem_addr;
-    output                      mem_read_en, mem_write_en;
-    output  [(B_WIDTH/8)-1:0]   write_byte_en;
-    inout   [B_WIDTH-1:0]       mem_data;
+module TRV32I_core (
+    input           clk, rst;
+    input   [31:0]  inst;
+    output  [31:0]  pc;
+    output  [31:0]  mem_addr;
+    output          mem_read_en, mem_write_en;
+    output  [3:0]   write_byte_en;
+    inout   [31:0]  mem_data;
 );
 
     //datapath
--- TRV-32I/top/TRV32I_top.sv
+++ TRV-32I/top/TRV32I_top.sv
@@ -1,5 +1,6 @@
 module TRV32I_top #(
     parameter B_WIDTH = 32;
+    parameter MEM_SIZE = 32;
 ) (
     input                   clk, rst;
     output  [31:0]          pc, inst;
@@ -11,26 +12,18 @@ module TRV32I_top #(
     wire [(B_WIDTH/8)-1:0]  write_byte_en;
 
 
-    TRV32I_core #(
-        .B_WIDTH(B_WIDTH)
-    ) TRV32I_core (
+    TRV32I_core TRV32I_core (
         .clk(clk), .rst(rst), .pc(pc), .inst(inst),
         .mem_addr(mem_addr), .mem_data(mem_data),
         .mem_read_en(mem_read_en), .mem_write_en(mem_write_en), .write_byte_en(write_byte_en)
     );
 
 
-    //below mem module will be replaced to mem_interface
-    mem_inst #(
-       .B_WIDTH(B_WIDTH)
-    ) mem_inst (
-        .clk(clk), .rst(rst), .pc(pc), .inst(inst)
-    );
-
-    mem_data #(
-       .B_WIDTH(B_WIDTH)
-    ) mem_data (
-        .clk(clk), .rst(rst),
+    mem_interface #(
+        .B_WIDTH(B_WIDTH)
+        .MEM_SIZE(MEM_SIZE)
+    ) mem_interface (
+        .clk(clk), .rst(rst), .pc(pc), .inst(inst),
         .mem_addr(mem_addr), .mem_data(mem_data),
         .mem_read_en(mem_read_en), .mem_write_en(mem_write_en), .write_byte_en(write_byte_en)
     );
